02271996048701243b84abb1cd573e3fa714080d
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-11 16:17:55 +0900

    style: footer add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Footer from 'components/common/layout/Footer';
 import Header from 'components/common/layout/Header';
 import Main from 'components/main/Main';
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
