a56bd216fa158e119a7f131dfc8ecc9515ebee10
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-12 22:24:05 +0900

    style: 프로필 링크 추가

--- components/common/Profile.tsx
+++ components/common/Profile.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
 import AutoHeightImageView from 'components/common/AutoHeightImageView';
+import { ReactNode } from 'react';
 
 import SAMPLE01 from 'src/assets/images/sample01.png';
+import TWITTER_24 from 'src/assets/icons/common/twitter_24.svg';
+import GITHUB_24 from 'src/assets/icons/common/github_24.svg';
+import MAIL_24 from 'src/assets/icons/common/mail_24.svg';
 
 const Profile = () => {
+  const socialLink = {
+    twitter: '#',
+    github: '#',
+    mail: '#',
+  };
   return (
     <div className='Profile'>
       <div className='Profile__img'>
@@ -16,9 +26,34 @@ const Profile = () => {
         <p className='Profile__introduce'>
           하이헬로안녕까꿍 에린의 핫섹시큐티 청순가련아이콘 샌드링이여요
         </p>
+        <ul className='Profile__social'>
+          <SocialListItem link={socialLink.twitter}>
+            <TWITTER_24 />
+          </SocialListItem>
+          <SocialListItem link={socialLink.github}>
+            <GITHUB_24 />
+          </SocialListItem>
+          <SocialListItem link={socialLink.mail}>
+            <MAIL_24 />
+          </SocialListItem>
+        </ul>
       </div>
     </div>
   );
+};
+
+const SocialListItem: React.FC<SocialListItemPropsType> = (props) => {
+  return (
+    <li className='Profile__social-item'>
+      <Link href={props.link} target='_blank'>
+        {props.children}
+      </Link>
+    </li>
+  );
+};
+type SocialListItemPropsType = {
+  link: string;
+  children: ReactNode;
 };
 
 export default Profile;
