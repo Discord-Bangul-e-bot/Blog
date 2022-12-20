04c5ecfe6e48d707a7329fe1cee0df35141f4700
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-21 01:14:35 +0900

    feat: useResize custom hook add

--- components/common/layout/Header.tsx
+++ components/common/layout/Header.tsx
@@ -1,15 +1,26 @@
 import Link from 'next/link';
 import { observer } from 'mobx-react';
 import useStore from 'store/useStore';
+import { useEffect } from 'react';
 
+import { useResize } from 'src/utils/utils';
+import mediaSize from 'src/styles/mediaSize';
 import Button from 'components/common/Button';
 import { SidebarMenu } from 'components/common/layout/Sidebar';
 import Category from 'components/common/Category';
 
 import SUN_24 from 'src/assets/icons/common/sun_24.svg';
+import CLOSE_24 from 'src/assets/icons/common/close_24.svg';
 import CLOSE_18 from 'src/assets/icons/common/close_18.svg';
 
 const Header: React.FC = () => {
+  // PARAM size 관련
+  const size = useResize();
+
+  useEffect(() => {
+    console.log(`width: ${size.width}, height: ${size.height}`);
+  }, [size.width, size.height]);
+
   const onClickMenuBtn = () => {
     useStore.headerStore.setIsOpenSidebar();
     return;
@@ -36,7 +47,7 @@ const Header: React.FC = () => {
               <SUN_24 />
             </Button>
           </div>
-          <button className='Header__menu-button'>
+          <button className='Header__menu-button' onClick={onClickMenuBtn}>
             <span className='Header__menu-bar'></span>
             <span className='Header__menu-bar'></span>
             <span className='Header__menu-bar'></span>
@@ -45,10 +56,17 @@ const Header: React.FC = () => {
         <div className={getIsSidebarClassName()}>
           <div className='Header__sidebar-inner'>
             <div className='Header__sidebar-close-container'>
-              <CLOSE_18
-                className='Header__sidebar-close-btn'
-                onClick={onClickMenuBtn}
-              />
+              {size.width && size.width > mediaSize.mobileSize ? (
+                <CLOSE_24
+                  className='Header__sidebar-close-btn'
+                  onClick={onClickMenuBtn}
+                />
+              ) : (
+                <CLOSE_18
+                  className='Header__sidebar-close-btn'
+                  onClick={onClickMenuBtn}
+                />
+              )}
             </div>
             <nav className='Header__sidebar-menu'>
               <Category />
