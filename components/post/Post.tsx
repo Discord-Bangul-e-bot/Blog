51498ab56dc49d0dcb15dbeb8f0354fa22404a26
Kimbangul <65225446+Kimbangul@users.noreply.github.com>
2022-12-27 00:07:44 +0900

    style: post info styling

--- components/post/Post.tsx
+++ components/post/Post.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link';
-
-import TagList from 'components/common/TagList';
+import PostInfo from 'components/post/PostInfo';
 
 const Post = () => {
   const postTagList = [
@@ -11,22 +9,7 @@ const Post = () => {
   ];
   return (
     <section className='Post'>
-      <div className='Post__info'>
-        <h2 className='Post__cate'>
-          <Link href='#'>샌드링 개발노트</Link>
-        </h2>
-        <div className='Post__title-container'>
-          <h1 className='Post__title'>방울이 발바닥으로 코딩하기</h1>
-          <div className='Post__comment-number'>0</div>
-        </div>
-        <dl className='Post__time'>
-          <dd className='Post__time-cate'>작성</dd>
-          <dt className='Post__time-item'>2022-10-01 19:09:33</dt>
-          <dd className='Post__time-cate'>수정</dd>
-          <dt className='Post__time-item'>2022-10-01 19:09:33</dt>
-        </dl>
-        <TagList className='Post__TagList' list={postTagList} />
-      </div>
+      <PostInfo />
     </section>
   );
 };
